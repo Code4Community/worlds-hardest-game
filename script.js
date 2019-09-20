2dacbea5934742b245c35e7148f3f75dd2b53825
Max Gruber <maxdg99@gmail.com>
2019-09-19 20:53:20 -0400

    init js commit

---
+++ script.js
@@ -0,0 +1,20 @@
+var context;
+var obstacles = [];
+
+document.addEventListener("DOMContentLoaded", function() {
+    var canvas = document.getElementById("board");
+    canvas.width = 800;
+    canvas.height = 600;
+    context = canvas.getContext("2d");
+}, false);
+
+function Obstacle(image, width, height, speed, startPixel, endPixel)
+{
+    this.image = image;
+    this.width = width;
+    this.height = height;
+    this.speed = speed;
+    this.startPixel = startPixel;
+    this.endPixel = endPixel;
+}
+
