37f7d1cb93954d2bf03be52fb4e9aabc4addb9d8
Max Gruber <maxdg99@gmail.com>
2019-10-24 20:27:03 -0400

    fixing code

--- script.js
+++ script.js
@@ -30,14 +30,12 @@ function setUpGame() {
     }
 
     aleksFace = "https://scontent-sea1-1.cdninstagram.com/vp/e92b43bf1d1c01d0a298e3937733c06c/5DD9D0C6/t51.2885-19/s150x150/52486763_624841137975557_4315053367689740288_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com";
-    obstacleData = [
-        // Format for each entry is as follows: 
-        // image URL, speedX, speedY, topLimitX, bottomLimitX, topLimitY, bottomLimitY, currentX, currentY
-        [aleksFace,   0,      0.2,    0,         1200,         10,        500,          150,      10],
-        [aleksFace,   0,      0.2,    0,         1200,         10,        500,          700,      10]
+
+    obstacles = [
+        //image URL, image widht, image height, speed X, speed Y, start X, start Y, end X, end Y
+        new Obstacle(aleksFace, 20, 20, 6, 6, 10, 10, 1100, 400, 11, 11)
     ];
-    obstacles = [];
-    oldTimeStamp = 0.0;
+
     xMin = 0;
     xMax = 1200; 
     yMin = 0;
@@ -48,16 +46,6 @@ function setUpGame() {
     canvas.height = yMax;
     context = canvas.getContext("2d");
 
-    // Constructs obstacles from array of obstacle data
-    parseObstacles(obstacleData);
-
-    // Debugging statements
-    console.log("Parsed obstacles after setUpGame(): ");
-    console.log(obstacles);
-
-    // window.requestAnimationFrame runs and then runs the function you pass to it
-    // Since gameLoop calls window.requestAnimationFrame(gameLoop) it will run forever
-    animationFrameId = window.requestAnimationFrame(gameLoop);
 }
 
 // Creates a 2D Point within the bounds of the screen
@@ -77,42 +65,16 @@ class Point {
 
 // Constructor for an Obstacle
 class Obstacle {
-    constructor(image, speedX, speedY, startPoint, endPoint, currentPoint) {
+    constructor(image, imageWidth, imageHeight, speedX, speedY, startPointX, startPointY, endPointX, endPointY, currentPointX, currentPointY) {
         //the this.image is an image object
         this.image = image;
+        this.imageWidth = imageWidth;
+        this.imageHeight = imageHeight;
         this.speedX = speedX;
         this.speedY = speedY;
-        // All of these are Point's
-        this.startPoint = startPoint;
-        this.currentPoint = currentPoint;
-        this.endPoint = endPoint;
-    }
-}
-
-// Takes an array of obstacles data
-//     Format for each obstacle data entry is as follows: 
-//     image URL, speedX, speedY, topLimitX, bottomLimitX, topLimitY, bottomLimitY, currentX, currentY
-// Constructs an Obstacle object from this
-// Pushes this object to the array of obstacles used in the canvas
-function parseObstacles(obstacleData)
-{
-    //image URL, imageWidth, imageHeight, speedX, speedY, topLimitX, bottomLimitX, topLimitY, bottomLimitY, currentX, currentY
-    for(var i = 0; i < obstacleData.length; i++)
-    {
-        // Creates an image object with size and imageUrl
-        var img = new Image(20, 20);
-        img.src = obstacleData[i][0];
-
-        // Constructs points for obstacle location and limits
-        var startPoint = new Point(obstacleData[i][3], obstacleData[i][5]);
-        var endPoint = new Point(obstacleData[i][4], obstacleData[i][6]);
-        var currentPoint = new Point(obstacleData[i][7], obstacleData[i][8]);
-
-        // Constructs the obstacle object from data created above
-        var currentObstacle = new Obstacle(img, obstacleData[i][1], obstacleData[i][2], startPoint, endPoint, currentPoint);
-        
-        // Pushes new obstacle to array
-        obstacles.push(currentObstacle);
+        this.startPoint = new Point(startPointX, startPointY);
+        this.currentPoint = new Point(currentPointX, currentPointY);
+        this.endPoint = new Point(endPointX - imageWidth, endPointY - imageHeight);
     }
 }
 
@@ -122,7 +84,7 @@ function gameLoop(timeStamp)
     oldTimeStamp = timeStamp;
     updateObstaclePositions(timePassed);
 
-    // See comments above in setUpGame for a quick explanation of .requestAnimationFrame()
+    // See comments above in setUpGame for a quick explanation of .requestAnimationFrame() 
     // since gameLoop is a function, and since .requestAnimationFrame calls the function passed into it, 
     //  gameLoop is a recursive function implemented across two functions
     animationFrameId = window.requestAnimationFrame(gameLoop);
@@ -130,7 +92,7 @@ function gameLoop(timeStamp)
 
 function updateObstaclePositions(timePassed)
 {
-    context.clearRect(0, 0, canvas.width, canvas.height);
+    clearCanvas();
     for(var i = 0; i < obstacles.length; i++)
     {
         //calculate future position of the obstacle
@@ -146,12 +108,17 @@ function updateObstaclePositions(timePassed)
             obstacles[i].speedY *= -1;
         }
         //draw the obstacle
-        drawImage(obstacles[i].image, obstacles[i].currentPoint);
+        drawObstacle(obstacles[i].image, obstacles[i].currentPoint);
     }
 }
 
+function clearCanvas()
+{
+    context.clearRect(0, 0, canvas.width, canvas.height);
+}
+
 // Draws an Image object on the canvas at the given Point (another object)
-function drawImage(image, point)
+function drawObstacle(image, point)
 {
     // Draw the image at the point
     context.drawImage(image, point.x, point.y);
