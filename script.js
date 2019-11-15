dd02446d901343cbd925ecddeedc7fdfada9d857
Max Gruber <maxdg99@gmail.com>
2019-11-14 20:14:29 -0500

    Things kinda work now

--- script.js
+++ script.js
@@ -13,6 +13,7 @@ var player;
 var obstacles;
 var level;
 var course;
+var objective;
 
 // Information for setInterval() and clearInterval()
 var intervalId;
@@ -88,24 +89,38 @@ function startGame() {
     
     var andrewFace = "https://greenecounty.alumni.osu.edu/wp-content/uploads/sites/25/2018/06/Andrew-Haberlandt.jpg";
     var aleksFace = "https://scontent-sea1-1.cdninstagram.com/vp/e92b43bf1d1c01d0a298e3937733c06c/5DD9D0C6/t51.2885-19/s150x150/52486763_624841137975557_4315053367689740288_n.jpg?_nc_ht=scontent-sea1-1.cdninstagram.com";
+    var blockO = "https://i.pinimg.com/originals/e6/93/9b/e6939b21548bcf2d2a98b150c3867cdb.png"
 
-    player = new Player(andrewFace, 100, 100, 3, 10, 10);
+    player = new Player(andrewFace, 80, 80, 3, 10, 10);
+    objective = new Objective(blockO, 80, 80, new Point(1020, 210));
 
     if(level === 1)
     {
         course = [
             //width, length, x, y, color
-            new Rectangle(50, 50, 100, 100, "blue")
+            // new Rectangle(1100, 50, 50, 30, "black"),
+            // new Rectangle(50, 400, 1150, 30, "black"),
+            // new Rectangle(1150, 50, 50, 430, "black")
         ];
 
         obstacles = [
-            //image URL, image widht, image height, speed X, speed Y, start X, start Y, end X, end Y, current X, current Y
-            new Obstacle(aleksFace, 100, 100, 0, .25, 10, 10, 1100, 500, 11, 11),
-            new Obstacle(aleksFace, 100, 100, .2, .25, 10, 10, 1100, 500, 11, 11)
+            new Obstacle(aleksFace, 80, 80, 0, .25, 0, 0, 1200, 500, 100, 0),
+            new Obstacle(aleksFace, 80, 80, 0, -.25, 0, 0, 1200, 500, 300, 420),
+            new Obstacle(aleksFace, 80, 80, .1, .2, 0, 0, 1200, 500, 600, 0),
+            new Obstacle(aleksFace, 80, 80, -.1, .2, 0, 0, 1200, 500, 900, 0)
         ];
     }
 
     intervalId = setInterval(updateGameState, updateInterval);
+}
+
+class Objective {
+    constructor(image, width, height, point)
+    {
+        this.image = new Image(width, height)
+        this.image.src = image;
+        this.point = point;
+    }
 }
 
 // 2D point within bounds of screen
@@ -119,10 +134,10 @@ class Point {
 
     checkPoint() {
         if (this.x < xMin || this.x > xMax) {
-            alert("x-coordinate " + this.x + " is out of range, xMin = " + xMin + " and xMax = " + xMax);
+           // alert("x-coordinate " + this.x + " is out of range, xMin = " + xMin + " and xMax = " + xMax);
         }
         if (this.y < yMin || this.y > yMax) {
-            alert("y-coordinate " + this.y + " is out of range, yMin = " + yMin + " and yMax = " + yMax);
+            //alert("y-coordinate " + this.y + " is out of range, yMin = " + yMin + " and yMax = " + yMax);
         }
     }
     
@@ -179,51 +194,90 @@ class Player {
     }
 
     moveRight() {
-        if (playerOnCourse(this.currentPoint.x + player.speed, this.currentPoint.y))
+        if (this.currentPoint.x  + player.speed <= xMax - this.image.width)
         {
             this.currentPoint.addX(player.speed);
         }
     }
 
     moveLeft() {
-        if (playerOnCourse(this.currentPoint.x - player.speed, this.currentPoint.y))
+        if (this.currentPoint.x - player.speed >= xMin)
         {
             this.currentPoint.subtractX(player.speed);
         }
     }
 
     moveUp() {
-        if (playerOnCourse(this.currentPoint.x, this.currentPoint.y - player.speed))
+        if (this.currentPoint.y - player.speed >= yMin)
         {
             this.currentPoint.subtractY(player.speed);
         }
     }
 
     moveDown() {
-        if (playerOnCourse(this.currentPoint.x, this.currentPoint.y + player.speed))
+        if (this.currentPoint.y + player.speed <= yMax - this.image.height)
         {
             this.currentPoint.addY(player.speed);
         }
     }
 }
 
-function playerOnCourse(x, y)
-{
-    //make sure the player does not collide with course here
-    var onCourse = false;
-   
-    if(y > 0 && x > 0 && y < yMax + player.image.height && x < xMax + player.image.width)
-    {
-        onCourse = true;
-    }
-
-    return onCourse;
-}
-
 function updateGameState() {
     moveAndDrawObstacles();
     moveAndDrawPlayer();
-    drawCourse()
+    drawCourse();
+    drawObjective();
+    atObjective();
+    hitObstacle();
+}
+
+function atObjective() {
+    var objectiveLeft = objective.point.x;
+    var objectiveRight = objective.point.x + objective.image.width;
+    var objectiveTop = objective.point.y;
+    var objectiveBottom = objective.point.y + objective.image.height;
+
+    var points = [new Point(player.currentPoint.x, player.currentPoint.y), 
+        new Point(player.currentPoint.x + player.image.width, player.currentPoint.y),
+        new Point(player.currentPoint.x, player.currentPoint.y + player.image.height),
+        new Point(player.currentPoint.x + player.image.width, player.currentPoint.y + player.image.height)]
+
+    for(var i = 0; i < 4; i++){
+        var point = points[i];
+        if(objectiveLeft < point.x && point.x < objectiveRight && objectiveTop < point.y && point.y < objectiveBottom)
+        {
+            stopGame();
+            clearCanvas();
+            context.font = "30px Arial";
+            context.fillText("You Won!!!", 600, 250);    
+        }
+    }
+}
+
+function hitObstacle() {
+    for(var n = 0; n < obstacles.length; n++) {
+        var obstacle = obstacles[n];
+        var obstacleLeft = obstacle.currentPoint.x;
+        var obstacleRight = obstacle.currentPoint.x + objective.image.width;
+        var obstacleTop = obstacle.currentPoint.y;
+        var obstacleBottom = obstacle.currentPoint.y + objective.image.height;
+
+        var points = [new Point(player.currentPoint.x, player.currentPoint.y), 
+            new Point(player.currentPoint.x + player.image.width, player.currentPoint.y),
+            new Point(player.currentPoint.x, player.currentPoint.y + player.image.height),
+            new Point(player.currentPoint.x + player.image.width, player.currentPoint.y + player.image.height)]
+
+        for(var i = 0; i < 4; i++){
+            var point = points[i];
+            if(obstacleLeft < point.x && point.x < obstacleRight && obstacleTop < point.y && point.y < obstacleBottom)
+            {
+                stopGame();
+                clearCanvas();
+                context.font = "30px Arial";
+                context.fillText("You Lost!!!", 600, 250);    
+            }
+        }
+    }
 }
 
 function moveAndDrawPlayer() {
@@ -268,6 +322,11 @@ function drawCourse()
         context.fillStyle = rect.color;
         context.fillRect(rect.position.x, rect.position.y, rect.width, rect.height);
     }
+}
+
+function drawObjective()
+{
+    drawImage(objective.image, objective.point);
 }
 
 function clearCanvas() {
