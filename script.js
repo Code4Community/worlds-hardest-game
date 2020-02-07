ede4a1933c1a832ae987cc86801ede412f13466a
Adam Lis <adambricelis@gmail.com>
2020-02-06 20:29:22 -0500

    Fixed duplicate function definitions in script.js

--- script.js
+++ script.js
@@ -44,8 +44,8 @@ function loadGame() {
     canvas.width = X_MAX;
     canvas.height = Y_MAX;
     context = canvas.getContext("2d");
-    
-    intervalId = null; 
+
+    intervalId = null;
 
     // Adds action listener for level select.
     document.getElementById('level-select').addEventListener('change', (e) => {
@@ -74,7 +74,7 @@ function startGame() {
         The top left corner is (0,0) and the bottom left corner is (1200, 500).
         Ask us if this confuses you!
     */
-    
+
     /*
         These are the url's to the images for different parts of the game.
         You can change them to different image URL's from the internet.
@@ -82,7 +82,7 @@ function startGame() {
     */
     var playerLogoUrl = "./assets/OhioStateLogo.jpg";
     var obstacleLogoUrl = "./assets/MichiganLogo.png";
-    var goalImageUrl = "./assets/Goal.png"
+    var goalImageUrl = "./assets/EndZone.png"
 
     /*
         The player is the person that you move around the screen and try to get to the endzone.
@@ -104,13 +104,13 @@ function startGame() {
         The thrid item is the height of the goal image.
         The fourth item is the point where the goal image is drawn.
     */
-    goal = new Objective(goalImageUrl, 75, 100, new Point(1090, 210));
+    goal = new Objective(goalImageUrl, 125, 500, new Point(1075, 0));
 
     // This is the select to switch between levels.  We suggest that you do not mess with this.
     var levelDropdown = document.getElementById("level-select");
     level = levelDropdown.options[levelDropdown.selectedIndex].value;
 
-    if(level == 1) {        
+    if (level == 1) {
         obstacles = [
             /*  
                 The first item in the parentheses is the url of the obstacle image.  You can find that above if you want to change it.
@@ -129,7 +129,7 @@ function startGame() {
             new Obstacle(obstacleLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(500, 50), 0, .11)
         ];
     }
-    else if(level == 2) {
+    else if (level == 2) {
         obstacles = [
             new Obstacle(obstacleLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, new Point(100, 0), new Point(1200, 500), new Point(100, 0), .1, .2),
             new Obstacle(obstacleLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, new Point(300, 0), new Point(1200, 500), new Point(300, 0), .1, .2),
@@ -142,8 +142,7 @@ function startGame() {
 
         ];
     }
-    else if(level == 3)
-    {
+    else if (level == 3) {
         obstacles = [
             new Obstacle(obstacleLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(0, 120), 1, 0),
             new Obstacle(obstacleLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(0, 320), 1, 0),
@@ -183,7 +182,7 @@ class Point {
             alert("y-coordinate " + this.y + " is out of range, yMin = " + Y_MIN + " and yMax = " + Y_MAX);
         }
     }
-    
+
     subtractX(subtractFromX) {
         this.x -= subtractFromX;
     }
@@ -233,107 +232,8 @@ document.addEventListener('keyup', (e) => {
     }
 });
 
-function loadGame() {
-    
-    canvas = document.getElementById("board");
-    canvas.width = X_MAX;
-    canvas.height = Y_MAX;
-    context = canvas.getContext("2d");
-    
-    intervalId = null; 
-
-    document.getElementById('level-select').addEventListener('change', (e) => {
-        stopGame();
-        clearCanvas();
-        let playInnerHtml = "<i class='material-icons float-left'>play_arrow</i>&nbsp;Play";
-        document.getElementById("playRestartButton").innerHTML = playInnerHtml;
-    });
-}
-
-function stopGame() {
-    if (intervalId != null) {
-        clearInterval(intervalId);
-    }
-    intervalId = null;
-}
-
-function startGame() {
-    stopGame();
-    
-    var ohioStateLogoUrl = "./assets/OhioStateLogo.jpg";
-    var michiganLogoUrl = "./assets/MichiganLogo.png";
-    var goalImageUrl = "./assets/EndZone.png"
-
-    player = new Player(ohioStateLogoUrl, 80, 80, 2, new Point(10, 10));
-    goal = new Objective(goalImageUrl, 125, 500, new Point(1075, 0));
-
-    var levelDropdown = document.getElementById("level-select");
-    level = levelDropdown.options[levelDropdown.selectedIndex].value;
-
-    if(level == 1)
-    {
-        course = [];
-        
-        let currentPointOne = new Point(100, 0);
-        let currentPointTwo = new Point(300, 420);
-        let currentPointThree = new Point(600, 0);
-        let currentPointFour = new Point(900, 0);
-        let currentPointFive = new Point(500, 50);
-
-        obstacles = [
-            // startPoint, endPoint, currentPoint
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, currentPointOne, 0, .17),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, currentPointTwo, 0, .15),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, currentPointThree, 0, .13),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, currentPointFour, 0, .12),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, currentPointFive, 0, .11)
-        ];
-    }
-    else if(level == 2)
-    {
-        course = [];
-
-        obstacles = [
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(100, 0), new Point(1200, 500), new Point(100, 0), .1, .2),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(300, 0), new Point(1200, 500), new Point(300, 0), .1, .2),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(500, 0), new Point(1200, 500), new Point(500, 0), .1, .2),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(700, 0), new Point(1200, 500), new Point(700, 0), .1, .2),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(500, 0), new Point(1200, 500), new Point(500, 0), .1, .2),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(0, 400), new Point(1200, 500), new Point(1000, 400), .5, 0),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(0, 300), new Point(1200, 500), new Point(1000, 300), .5, 0),
-            new Obstacle(michiganLogoUrl, 80, 80, new Point(0, 200), new Point(1200, 500), new Point(1000, 200), .5, 0),
-
-        ];
-    }
-    else if(level == 3)
-    {
-        course = [];
-
-        obstacles = [
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(0, 120), 1, 0),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(0, 320), 1, 0),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(100, 0), 0, .75),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(300, 0), 0, .8),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(500, 0), 0, .85),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(700, 0), 0, .9),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(900, 0), 0, .95),
-            new Obstacle(michiganLogoUrl, OBSTACLE_SIZE, OBSTACLE_SIZE, topLeftOriginPoint, defaultEndPoint, new Point(1100, 0), 0, 1),
-        ];
-    }
-    else {
-        console.log("No level selected. ");
-    }
-
-    let restartInnerHtml = "<i class='material-icons float-left'>replay</i>Restart";
-
-    document.getElementById("playRestartButton").innerHTML = restartInnerHtml;
-
-    intervalId = setInterval(updateGameState, updateInterval);
-}
-
 class Objective {
-    constructor(image, width, height, point)
-    {
+    constructor(image, width, height, point) {
         this.image = new Image(width, height)
         this.image.src = image;
         this.point = point;
@@ -363,29 +263,25 @@ class Player {
     }
 
     moveRight() {
-        if (this.currentPoint.x  + player.speed <= X_MAX - this.image.width)
-        {
+        if (this.currentPoint.x + player.speed <= X_MAX - this.image.width) {
             this.currentPoint.addX(player.speed);
         }
     }
 
     moveLeft() {
-        if (this.currentPoint.x - player.speed >= X_MIN)
-        {
+        if (this.currentPoint.x - player.speed >= X_MIN) {
             this.currentPoint.subtractX(player.speed);
         }
     }
 
     moveUp() {
-        if (this.currentPoint.y - player.speed >= Y_MIN)
-        {
+        if (this.currentPoint.y - player.speed >= Y_MIN) {
             this.currentPoint.subtractY(player.speed);
         }
     }
 
     moveDown() {
-        if (this.currentPoint.y + player.speed <= Y_MAX - this.image.height)
-        {
+        if (this.currentPoint.y + player.speed <= Y_MAX - this.image.height) {
             this.currentPoint.addY(player.speed);
         }
     }
@@ -411,56 +307,54 @@ function atObjective() {
 
     // This gets bounding box of where the player is
     var points = [
-        new Point(player.currentPoint.x, player.currentPoint.y), 
+        new Point(player.currentPoint.x, player.currentPoint.y),
         new Point(player.currentPoint.x + player.image.width, player.currentPoint.y),
         new Point(player.currentPoint.x, player.currentPoint.y + player.image.height),
         new Point(player.currentPoint.x + player.image.width, player.currentPoint.y + player.image.height)
     ]
 
     // This loops through the array of points to see if the 
-    for(var i = 0; i < 4; i++){
+    for (var i = 0; i < 4; i++) {
         var point = points[i];
         // This checks to see if the two bounding boxes overlap
-        if(objectiveLeft < point.x && point.x < objectiveRight && objectiveTop < point.y && point.y < objectiveBottom)
-        {
+        if (objectiveLeft < point.x && point.x < objectiveRight && objectiveTop < point.y && point.y < objectiveBottom) {
             // This is what happens if the player wins
             stopGame();
             clearCanvas();
-            if(level != 3) {
+            if (level != 3) {
                 document.getElementById('nextLevelButton').style.display = "inline";
             }
             context.font = "72px Arial";
             context.fillStyle = "red";
             context.textAlign = "center";
-            context.fillText("TOUCHDOWN!", canvas.width / 2, canvas.height / 2);    
+            context.fillText("TOUCHDOWN!", canvas.width / 2, canvas.height / 2);
         }
     }
 }
 
 function hitObstacle() {
-    for(var n = 0; n < obstacles.length; n++) {
+    for (var n = 0; n < obstacles.length; n++) {
         var obstacle = obstacles[n];
         var obstacleLeft = obstacle.currentPoint.x;
         var obstacleRight = obstacle.currentPoint.x + obstacle.image.width;
         var obstacleTop = obstacle.currentPoint.y;
         var obstacleBottom = obstacle.currentPoint.y + obstacle.image.height;
 
-        var points = [new Point(player.currentPoint.x, player.currentPoint.y), 
-            new Point(player.currentPoint.x + player.image.width, player.currentPoint.y),
-            new Point(player.currentPoint.x, player.currentPoint.y + player.image.height),
-            new Point(player.currentPoint.x + player.image.width, player.currentPoint.y + player.image.height)]
+        var points = [new Point(player.currentPoint.x, player.currentPoint.y),
+        new Point(player.currentPoint.x + player.image.width, player.currentPoint.y),
+        new Point(player.currentPoint.x, player.currentPoint.y + player.image.height),
+        new Point(player.currentPoint.x + player.image.width, player.currentPoint.y + player.image.height)]
 
-        for(var i = 0; i < 4; i++){
+        for (var i = 0; i < 4; i++) {
             var point = points[i];
-            if(obstacleLeft < point.x && point.x < obstacleRight && obstacleTop < point.y && point.y < obstacleBottom)
-            {
+            if (obstacleLeft < point.x && point.x < obstacleRight && obstacleTop < point.y && point.y < obstacleBottom) {
                 debugger
                 stopGame();
                 clearCanvas();
                 context.font = "72px Arial";
                 context.fillStyle = "blue";
                 context.textAlign = "center";
-                context.fillText("You got tackled!", canvas.width / 2, canvas.height / 2);   
+                context.fillText("You got tackled!", canvas.width / 2, canvas.height / 2);
             }
         }
     }
@@ -483,13 +377,12 @@ function moveAndDrawPlayer() {
 }
 
 function moveAndDrawObstacles() {
-    for (var i = 0; i < obstacles.length; i++)
-    {
+    for (var i = 0; i < obstacles.length; i++) {
         //calculate future position of the obstacle
         obstacles[i].currentPoint.addX(obstacles[i].speedX * updateInterval);
         obstacles[i].currentPoint.addY(obstacles[i].speedY * updateInterval);
-        
-        if (obstacles[i].currentPoint.x < obstacles[i].startPoint.x || obstacles[i].currentPoint.x > obstacles[i].endPoint.x || obstacles[i].currentPoint.y < obstacles[i].startPoint.y || obstacles[i].currentPoint.y > obstacles[i].endPoint.y){
+
+        if (obstacles[i].currentPoint.x < obstacles[i].startPoint.x || obstacles[i].currentPoint.x > obstacles[i].endPoint.x || obstacles[i].currentPoint.y < obstacles[i].startPoint.y || obstacles[i].currentPoint.y > obstacles[i].endPoint.y) {
             obstacles[i].speedX *= -1;
             obstacles[i].speedY *= -1;
         }
@@ -499,8 +392,7 @@ function moveAndDrawObstacles() {
     }
 }
 
-function drawObjective()
-{
+function drawObjective() {
     drawImage(goal.image, goal.point);
 }
 
