ef4f3f75ab861725b7840bb327fbe547b962c1f1
Adam Lis <adambricelis@gmail.com>
2020-02-13 20:32:24 -0500

    Refactored selected variables into workshop.js

---
+++ workshop.js
@@ -0,0 +1,31 @@
+// Player image (the Ohio State logo)
+var playerLogoUrl = "./assets/OhioStateLogo.jpg";
+
+// Player size
+var playerSize = 80;
+
+// Player speed
+var playerSpeed = 2;
+
+// Obstacle image (the Michigan logos)
+var obstacleLogoUrl = "./assets/MichiganLogo.png";
+
+// Obstacle size
+var obstacleSize = 80;
+
+// Message when you win
+var winningText = "TOUCHDOWN!";
+
+// Color of message when you win
+var winningColor = "red";
+
+// Message when you lose
+var losingColor = "You got tackled!";
+
+// Color of message when you lose
+var losingColor = "blue";
+
+// Goal image
+var goalImageUrl = "./assets/EndZone.png";
+
+// Background image (WIP)
